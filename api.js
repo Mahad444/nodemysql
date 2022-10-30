2033e5792038f2f15b20df99f57de4eecaa0c5bc
Mahad444 <mahadsaid95@icloud.com>
2022-10-30 22:37:38 +0300

    connecting databases

--- api.js
+++ api.js
@@ -1,1 +1,33 @@
 
+
+// var mysql = require('mysql');
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "myusername",
+//   password: "mypassword",
+//   database:"nodemysql"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   /*Create a database named "mydb":*/
+
+
+// const express = require ("express")
+
+// const app = express();
+// app.listen(6000, ()=>{
+//     console.log("now listening to port http://localhost:6000")
+// })
+
+// app.get('/create',(req,req)=>{
+//     con.query("CREATE DATABASE nodemysql", function (err, result) {
+//         if (err) throw err;
+//         console.log("Database created");
+//       });
+//     });
+
+// })
+
--- index.js
+++ index.js
@@ -3,36 +3,36 @@ const mysql = require ("mysql")
 
 // Connection of the DB
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Qwerty123"
+
+    host :"127.0.0.1",
+    user :"root",
+    password :"root",
+    port:'3306',
+
 })
 
 // Connction
 db.connect((err)=>{
-    if(err){
-        throw err;
-    }
+    if(err) throw err;
     console.log("MySQL database Connected")
-})
+});
 
 
 const app = express();
 
+app.get('/mydb' ,(req,res)=>{
+    db.query("CREATE DATABASE nodemysql",(err,result)=>{
+        if(err) throw err;
+        console.log(result)
+        res.send("Connected db")
+    })
+})
 
 app.listen(8000,() =>{
     console.log("App Listening to http://localhost:8000")
 })
 
-app.get('/createddb' ,(req,res)=>{
-    let sql ="CREATE DATABASE nodemysql"
-    db.query(sql,(err,result)=>{
-        if(err){
-            throw err;
-        }
-        res.send("Connected db")
-    })
-})
 
 
+module.exports = db;
 
